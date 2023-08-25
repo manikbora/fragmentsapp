3be083afcac661ec7c206478a2b0f93e80e6470a
Manik Bora <manikbora@gmail.com>
2023-08-25 10:45:55 +0530

    Soft keyboard hide functionality implemented

--- app/src/main/java/com/example/fragmentsapp/Communicator.kt
+++ app/src/main/java/com/example/fragmentsapp/Communicator.kt
@@ -3,7 +3,7 @@ package com.example.fragmentsapp
 import androidx.fragment.app.Fragment
 
 interface Communicator {
-    fun saveData(userName: String, password: String)
+    fun saveData(name: String, password: String)
     fun fetchData(name: String, password: String): Boolean
     fun fetchData(): String
     fun setFragment(frag: Fragment)
